d2be0e78d01ed1fcca9ad49c606fd9bb98cec570
trabelsi1995 <oussama.trabelsi@polytechnicien.tn>
2020-10-25 02:53:05 -0700

    part 1 project corona

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    'mongodb+srv://oussamatrabelsi:X7jAeBHbYJ4IJVCC@cluster0.s0nvy.mongodb.net/merng?retryWrites=true&w=majority',
+  SECRET_KEY: 'some very secret key'
+};
